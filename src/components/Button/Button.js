1e4440137a9bf7f91029eec339b3dd6f3010144e
Jose Pedraza <pastorpedraza3@gmail.com>
2021-03-23 10:41:21 -0600

    removed linter diseable and fixed the linter

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,12 +1,13 @@
-/* eslint-disable  react/require-default-props */
 import PropTypes from 'prop-types';
 
 const Button = ({ name }) => <button type="button">{name}</button>;
+
+Button.defaultProps = {
+  name: null,
+};
 
 Button.propTypes = {
   name: PropTypes.string,
 };
 
 export default Button;
-
-/* eslint-enable  react/require-default-props */
