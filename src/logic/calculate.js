72079b35afa3142c7f6aa3f5746acd88acf2525e
Jose Pedraza <pastorpedraza3@gmail.com>
2021-03-23 12:45:57 -0600

    Implemented calculate.js

---
+++ src/logic/calculate.js
@@ -0,0 +1,10 @@
+const calculate = (data,btnName) => {
+    let total = null;
+    let next = null;
+    const operator = btnName;
+
+
+    if( operator == '+/-') { total *= -1; next *= -1 };
+}
+
+export default calculate;
