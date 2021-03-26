e8b713cb2ab8f1a6012f1461bfb5eecdc472ed04
Jose Pedraza <pastorpedraza3@gmail.com>
2021-03-25 19:14:56 -0600

    Added handleClick and bind it to Button Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,39 @@
-/* eslint-disable no-unused-vars */
+/* eslint-disable no-unused-vars, class-methods-use-this */
 import './App.css';
+import React from 'react';
 import Display from './Display/Display';
 import ButtonPanel from './ButtonPanel/ButtonPanel';
 import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <div>
-      <Display />
-      <ButtonPanel />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      total: null,
+      next: null,
+      operator: null,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(btnName) {
+    const { total, next, operator } = this.state;
+    console.log(total, next, operator);
+    console.log(btnName);
+  }
+
+  render() {
+    return (
+      <div>
+        <Display />
+        <ButtonPanel clickHandler={() => this.handleClick} />
+      </div>
+    );
+  }
 }
 
 export default App;
 
-/* eslint-disable no-unused-vars */
+/* eslint-enable no-unused-vars, class-methods-use-this */
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,13 +1,15 @@
 import PropTypes from 'prop-types';
 
-const Button = ({ name }) => <button type="button">{name}</button>;
+const Button = ({ name, click }) => <button type="button" onClick={() => click(name)}>{name}</button>;
 
 Button.defaultProps = {
   name: null,
+  click: null,
 };
 
 Button.propTypes = {
   name: PropTypes.string,
+  click: PropTypes.func,
 };
 
 export default Button;
