f94cd013c6ca64aff5c9bc1b917e84c65f19c056
Jose Pedraza <pastorpedraza3@gmail.com>
2021-03-31 18:48:54 -0600

    Add snapshot test for components

---
+++ src/components/__tests__/Home.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { BrowserRouter } from 'react-router-dom';
+
+import Home from '../Home/Home';
+
+it('renders correctly', () => {
+  const tree = renderer.create(<BrowserRouter><Home /></BrowserRouter>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
